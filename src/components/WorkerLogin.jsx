9e7a5b0dbbd7536b1c66516831c61756a785310f
Youssef5351 <155520872+Youssef5351@users.noreply.github.com>
2025-08-21 19:52:55 +0300

    Update WorkerLogin.jsx

--- src/components/WorkerLogin.jsx
+++ src/components/WorkerLogin.jsx
@@ -9,7 +9,7 @@ const Login = ({ setAuth }) => {
 
   const handleLogin = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/api/members/login", {
+      const res = await axios.post("https://patient-managment-backend.vercel.app/api/members/login", {
         username,
         password,
       });
